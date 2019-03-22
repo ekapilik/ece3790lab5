dcb47602b57edcb2d94fbb9a648a617aa398028e
ekapilik <erickapilik@gmail.com>
2019-03-22 09:04:33 -0500

    generate primes

--- nonwitness.py
+++ nonwitness.py
@@ -12,7 +12,7 @@ Created on Wed Mar 20 12:47:31 2019
         wrote non_witness_test function
         added simple tests
 """
-import sys, random as rand
+import sys, rsa, random as rand
 
 ###############################################################################
 """
@@ -31,12 +31,19 @@ def non_witness_test(x, s):
     nw = -1
     w = -1
     tested = []
+    test = 1
     
+    #if more tests than there are available numbers...
     if(s > x-2):
-        print("ERROR: Certainty is greater than x-2")
-        return False
+        s = x-3
+        
+    #Test 2 first because it's common
+    if( pow(2, x-1, x) == 1): #non-witness to compositeness
+        nw = 2
+    else: #witness to compositeness
+        w = 2
     
-    for test in range(s): #run test s times
+    for test in range(2, s + 2): #run test s times
         a = rand.randint(2, x-2) # select from Z+*
         while(tested.count(a) != 0): #get unique a
             a = rand.randint(2, x-2) # select from Z+*
@@ -51,17 +58,34 @@ def non_witness_test(x, s):
             break
     
     if(nw != -1 and w == -1):
-        print("%d is probabilistically prime (certainty: %d)" % (x,s))
+        #print("%d is probabilistically prime (certainty: %d)" % (x,s))
         return False
+    elif(nw == -1 and w != -1): #purely composite
+       # print("%d is purely composite" % x)
+       return False
     else: #only considered non-witness if there are also witnesses...
+        print("Count %d" % test)
         print("%d is composite" % x)
         print("\tWitnesses: %d" % w)
         print("\tNon-witnesses: %d" % nw)
         return True
-        
+
+def driver(nbits = 38):
+    done = False
+    test = 1
+    
+    while(not done):
+        prime = abs(rand.getrandbits(nbits))
+        done = non_witness_test(prime, 5)
+    
 ###############################################################################
 #########################      Tests       ####################################
 ###############################################################################    
-non_witness_test(5, 8) #should through error.. 
-non_witness_test(17, 5) #should be prime
-non_witness_test(15, 5) #should have non-witness
+#non_witness_test(5, 8) #should through error.. 
+#non_witness_test(17, 5) #should be prime
+#non_witness_test(15, 5) #should have non-witness
+#non_witness_test(27534692908801, 50)
+#non_witness_test(339772479099301, 50)
+
+driver()
+
